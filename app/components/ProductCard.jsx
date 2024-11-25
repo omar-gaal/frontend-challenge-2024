f584ac0cd2f36ff27755cf7e15cdab6c752ae349
omar-gaal <omargaal123@gmail.com>
2024-11-25 20:49:37 +0100

    testing github

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 const ProductCard = ({ product }) => {
     return (
+
+      
         <div className="product-card border p-4 rounded-lg shadow-md hover:shadow-lig">
              <Link href={`/product-page/${product.id}`}> 
             <img 
+            
             src={product.image} alt={product.title} 
             className="w-full h-56 object-cover mb-4" 
             />
@@ -12,6 +15,7 @@ const ProductCard = ({ product }) => {
             <p className="text-sm text-gray-500">{product.category}</p>
             <p className="text-xl font-bold text-blue-600">${product.price}</p>
             </Link>
+            <h1>Testing github </h1>
             <button className="mt-4 bg-blue-600 text-white py-2 px-4 rounded">
               Add to Cart
              </button>
