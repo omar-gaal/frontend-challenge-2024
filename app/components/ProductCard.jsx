2731be5df161234736482a05ecfcbed10b16ae97
omar-gaal <omargaal123@gmail.com>
2024-11-26 00:11:38 +0100

    optimize images

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from 'next/image'
 
 const ProductCard = ({ product }) => {
     return (
@@ -6,11 +7,16 @@ const ProductCard = ({ product }) => {
       
         <div className="product-card border p-4 rounded-lg shadow-md hover:shadow-lig">
              <Link href={`/product-page/${product.id}`}> 
-            <img 
-            
-            src={product.image} alt={product.title} 
-            className="w-full h-56 object-cover mb-4" 
-            />
+
+
+                <Image
+                src={product.image}
+                alt={product.title}                 
+                className="w-full h-56 object-cover mb-4" 
+                 width={500}  
+                height={500}  
+                />            
+        
             <h3 className="text-lg font-semibold">{product.title}</h3>
             <p className="text-sm text-gray-500">{product.category}</p>
             <p className="text-xl font-bold text-blue-600">${product.price}</p>
